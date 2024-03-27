256c0938a38c3c9aef0bc0cfdc846c85e1b1ff2b
c1ose-d <michael.glodin2004@gmail.com>
2024-03-27 22:03:27 +0300

    Add project files.

---
+++ ParsingTest/GetRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace ParsingTest;
+
+public class GetRequest
+{
+    public GetRequest(string requestUri)
+    {
+        RequestUri = requestUri;
+        GetRequestHtml();
+    }
+
+    private string RequestUri { get; set; } = null!;
+    public string Input { get; set; } = string.Empty;
+
+    private void GetRequestHtml()
+    {
+        using HttpClient client = new() { Timeout = TimeSpan.FromSeconds(60) };
+        using HttpResponseMessage httpResponseMessage = client.GetAsync(RequestUri).Result;
+        using HttpContent httpContent = httpResponseMessage.Content;
+        Input = httpContent.ReadAsStringAsync().Result;
+    }
+}
---
+++ ParsingTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static EdgeDriver Driver { get; set; } = null!;
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            EdgeDriverService driverService = EdgeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            Driver = new EdgeDriver(driverService, new EdgeOptions())
+            {
+                Url = "https://www.citilink.ru/catalog/noutbuki/"
+            };
+
+            string s = Driver.PageSource;
+            Regex regex = new(@"<a href=""/product/(?<space>.*?) e1259i3g0"">(?<val>.*?)</a>", RegexOptions.Compiled | RegexOptions.Singleline);
+            MatchCollection matchCollection = regex.Matches(s);
+            Console.WriteLine(matchCollection.Count);
+            foreach (Match match in matchCollection)
+            {
+                Console.WriteLine(match.Value.Split("\"")[3].Replace("&quot;", ""));
+            }
+
+        }
+        catch { }
+
+
+        //try
+        //{
+        //    string url = $"https://www.citilink.ru/catalog/noutbuki/";
+        //    Driver.Navigate().GoToUrl(url);
+        //    Thread.Sleep(10000);
+
+        //    for (int i = 1; i <= 20; i++)
+        //        try
+        //        {
+        //            ReadOnlyCollection<IWebElement> elements = Driver.FindElements(By.ClassName("e1259i3g0"));
+        //            Thread.Sleep(10000);
+        //            foreach (IWebElement element in elements)
+        //            {
+        //                element.
+        //            }
+        //        }
+        //        catch { }
+        //}
+        //catch { }
+
+        Driver.Close();
+    }
+}
